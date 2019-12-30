14d765601b4e776a6a280d4f59e4259992681b84
lewelyn7 <progit8itakademia>
2019-12-30 19:08:39 +0100

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppCourseComponent } from './app-course/app-course.component';
+import { AppCourseListComponent } from './app-course-list/app-course-list.component';
+import { AngularFireModule } from '@angular/fire';
+import { CoreModule } from './core/core.module';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { AddCourseComponent } from './add-course/add-course.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CourseFilterComponent } from './course-filter/course-filter.component';
+import { filterPipe } from './app-course-list/filterPipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppCourseComponent,
+    AppCourseListComponent,
+    UserProfileComponent,
+    AddCourseComponent,
+    CourseFilterComponent,
+    filterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //AngularFireModule.initializeApp();
+    CoreModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
